9ee98617efb491902309312232d8bc882dd5f112
AnaCorona23 <89944109+AnaCorona23@users.noreply.github.com>
2021-10-06 14:28:40 -0500

    Add files via upload

---
+++ ejercicios.js
@@ -0,0 +1,47 @@
+//ejercicio1
+var  numeros  =  0 ;
+var  a  =  0 ;
+var  b ;
+    para ( var  i  =  0 ;  i  <=  100 ;  i ++ ) {
+        a =  numeros ;
+        numeros  =  numeros  +  i ;
+        b  =  yo ;
+         consola . log ( "La suma de"  +  a  +  "+"  +  b  +  "es:"  +  numeros ) ;
+    }
+ //ejercicio2
+    var  numeros  =  0 ;
+var  a  =  0 ;
+var  b ;
+    para ( var  i  =  0 ;  i  <=  100 ;  i ++ ) {
+        a =  numeros ;
+        numeros  =  numeros  +  i ;
+        b  =  yo ;
+         consola . log ( "La suma de"  +  a  +  "+"  +  b  +  "es:"  +  numeros ) ;
+    }
+ //ejercicio3
+    var  decimales  =  0 ;
+
+para ( var  i  =  0 ;  i  <=  9 ;  i ++ ) {
+    
+    decimales  =  decimales  +  0.1 ;
+    decimales  =  decimales  -  1 ;
+    decimales ++ ;
+
+    consola . log ( decimales ) ;
+}
+//ejercicio 4
+numero = parseInt ( prompt ( "Ingresa el numero que desa saber su iteracion" ) ) ; 
+var  iteracion  =  numero ;
+var  resultado ;
+var  a  =  0 ;
+var  b ;
+var  suma ;
+    para ( var  i  =  0 ;  i  <  iteracion ;  i ++ ) {
+        b = parseInt ( prompt ( "Ingresa el numero para la iteracion" ) ) ; 
+        suma  =  a  +  b ;
+        a  =  suma ;  
+    }
+
+    consola . log ( "La suma es:"  +  suma ) ;
+
+    
